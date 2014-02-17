f134b7d47fe4a6cf446a06892ada07b0e42e1064
Alex Lauderdale <alauderdale@mac.com>
2014-02-16 18:19:52 -0700

    first

--- db/schema.rb
+++ db/schema.rb
@@ -26,7 +26,8 @@ ActiveRecord::Schema.define(version: 20140214221932) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "name"
+    t.string   "first_name",             default: "", null: false
+    t.string   "last_name",              default: "", null: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
