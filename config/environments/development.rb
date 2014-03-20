86fb18dee261a876cc406fa25f3dd6cd1d852c01
Alex Lauderdale <alauderdale@mac.com>
2014-03-19 23:10:37 -0600

    test 2 updates

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,13 +30,13 @@ ScotchClub::Application.configure do
 
     #image uploading
 
-  # config.paperclip_defaults = {
-  # :storage => :s3,
-  # :s3_credentials => {
-  #   :bucket => ENV['AWS_BUCKET'],
-  #   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-  #   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
-  #   }
-  # }
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
   
 end
