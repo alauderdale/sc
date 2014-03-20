c07f4bdf436ff9bb8655b8ff22b5d061a25f15ef
Alex Lauderdale <alauderdale@mac.com>
2014-03-19 22:52:25 -0600

    commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,4 +27,16 @@ ScotchClub::Application.configure do
   # number of complex assets.
   config.assets.debug = true
   Paperclip.options[:command_path] = "/usr/local/bin/"
+
+    #image uploading
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => ENV['AWS_BUCKET'],
+    :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+    :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
+  
 end
