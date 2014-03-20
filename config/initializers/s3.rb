94d83fcce626dff24d842de863fa25d4be5a6868
Alex Lauderdale <alauderdale@mac.com>
2014-03-19 23:23:01 -0600

    new bucket

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,6 +1,6 @@
 if Rails.env == "production"
   # set credentials from ENV hash
-  S3_CREDENTIALS = { :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], :bucket => "scotch-files"}
+  S3_CREDENTIALS = { :access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'], :bucket => "scotch-files-2"}
 else
   # get credentials from YML file
   S3_CREDENTIALS = Rails.root.join("config/s3.yml")
