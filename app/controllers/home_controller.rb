41178d6b44ac533d6e0fd8de34b4df0d9ac111e2
Alex Lauderdale <alauderdale@mac.com>
2014-03-28 01:41:57 -0600

    awesome

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,5 +2,6 @@ class HomeController < ApplicationController
   def index
     @users = User.all
     @check_ins = CheckIn.all(:order => 'updated_at DESC', :limit => 10)
+    @recent_liquors = Liquor.all(:limit => 10, :order => 'created_at DESC')
   end
 end
