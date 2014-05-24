db0c3ee0dd4f40984f6a0ffb7363c1d5c425fe0e
Alex Lauderdale <alauderdale@mac.com>
2014-05-24 01:45:02 -0600

    updating

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @users = User.all
-    @check_ins = CheckIn.order('created_at desc')
-    @recent_liquors = Liquor.all(:limit => 10, :order => 'created_at DESC')
+
+    if current_user
+    	@check_ins = current_user.check_ins.order('created_at desc')
+    	@recent_liquors = current_user.check_ins.joins(:liquor).select("liquors.*,  check_ins.*").order("liquors desc").limit(5)
+    	@user_liquors = current_user.checked_in_liquors.order("name asc").limit(5)
+  	end
+    # @recent_liquors = Liquor.all(:limit => 10, :order => 'created_at DESC')
   end
 end
