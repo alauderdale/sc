4a84ed8dfdea27dfbc1f84364136bb8973a3fe59
alauderdale <alauderdale@mac.com>
2014-05-28 20:06:54 -0600

    cool

--- app/helpers/check_ins_helper.rb
+++ app/helpers/check_ins_helper.rb
@@ -1,6 +1,3 @@
 module CheckInsHelper
-	def total_score
-    @check_in = CheckIn.find(params[:id])
-    @check_in.nose_rating + @check_in.taste_rating + @check_in.finish_rating + @check_in.rating
-  end
+
 end
